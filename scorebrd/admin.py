bc4b7774f070ec51c338c06c07e4cfe02dcbc2e5
mr.Shu <mr@shu.io>
2013-01-02 15:29:41 +0100

    added administration for things in scorebrd

---
+++ scorebrd/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from scorebrd.models import Team, Group, Competition, Match
+
+admin.site.register(Team)
+admin.site.register(Group)
+admin.site.register(Competition)
+admin.site.register(Match)
