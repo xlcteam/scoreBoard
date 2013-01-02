6f695da294640409ff7f61c6a86525e2db785a2e
Ado Matejov <a.matejov@centrum.sk>
2013-01-02 16:15:17 +0100

    added event

--- scorebrd/models.py
+++ scorebrd/models.py
@@ -3,17 +3,25 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=200)
 
-class Group(models.Model):
-    name = models.CharField(max_length=200)
-    teams = models.ManyToManyField(Team)
-
-class Competition(models.Model):
-    name = models.CharField(max_length=200)
-    groups = models.ManyToManyField(Group)
-    teams = models.ManyToManyField(Team)
 
 class Match(models.Model):
     teamA = models.ForeignKey(Team, related_name='homelanders')
     teamB = models.ForeignKey(Team, related_name='foreigners')
     scoreA = models.IntegerField(default=0)
     scoreB = models.IntegerField(default=0)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    teams = models.ManyToManyField(Team)
+
+
+class Competition(models.Model):
+    name = models.CharField(max_length=200)
+    groups = models.ManyToManyField(Group)
+    teams = models.ManyToManyField(Team)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    competitions = models.ManyToManyField(Competition)
