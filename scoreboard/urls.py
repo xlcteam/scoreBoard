b7067a0684e2a23be7f58d65fe88bdd908feb45a
Ado Matejov <a.matejov@centrum.sk>
2013-01-02 18:13:07 +0100

    added home view

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'scoreboard.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='Dashboard/default.html')),
     # url(r'^scoreboard/', include('scoreboard.foo.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
 
