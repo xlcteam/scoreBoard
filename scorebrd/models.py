544fa08e11c743ffaea65f627725523131b91447
mr.Shu <mr@shu.io>
2013-01-02 15:23:40 +0100

    added models

--- scorebrd/models.py
+++ scorebrd/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
 class Group(models.Model):
     name = models.CharField(max_length=200)
+    teams = models.ManyToManyField(Team)
 
 class Competition(models.Model):
     name = models.CharField(max_length=200)
     groups = models.ManyToManyField(Group)
-
-class Team(models.Model):
-    name = models.CharField(max_length=200)
-    competition = models.ForeignKey(Competition)
+    teams = models.ManyToManyField(Team)
 
 class Match(models.Model):
-    teamA = models.ForeignKey(Team)
-    teamB = models.ForeignKey(Team)
+    teamA = models.ForeignKey(Team, related_name='homelanders')
+    teamB = models.ForeignKey(Team, related_name='foreigners')
     scoreA = models.IntegerField(default=0)
     scoreB = models.IntegerField(default=0)
