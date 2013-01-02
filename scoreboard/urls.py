9fc6b008ba4c8e920eb358fc52061e80aa6ea403
mr.Shu <mr@shu.io>
2013-01-02 22:51:48 +0100

    updated views

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -8,7 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'scorebrd.views.index'),
     url(r'^teams/?$', 'scorebrd.views.teams'),
+    url(r'^team/(?P<team_id>\d+)/?$', 'scorebrd.views.team'),
     url(r'^events/?$', 'scorebrd.views.events'),
+    url(r'^event/(?P<event_id>\d+)/?$', 'scorebrd.views.event'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- scorebrd/views.py
+++ scorebrd/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from scorebrd.models import Team, Event, Group, Competition
 
 def events(request):
@@ -10,8 +10,16 @@ def teams(request):
     teams = Team.objects.all()
     return render_to_response('teams.html', {'teams': teams})
 
+def team(request, team_id):
+    team = get_object_or_404(Team, pk=team_id)
+    return render_to_response('team.html', {'team': team})
+
 def index(request):
     return render_to_response('index.html')
+
+def group(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    return render_to_response('group.html', {'group': group})
 
 def groups(request):
     groups = Group.objects.all()
