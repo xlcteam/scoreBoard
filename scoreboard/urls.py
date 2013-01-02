590ec27dbea779c91870e327064c058fb3294674
mr.Shu <mr@shu.io>
2013-01-02 18:31:57 +0100

    added teams listing

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -6,15 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scoreboard.views.home', name='home'),
     url(r'^$', TemplateView.as_view(template_name='Dashboard/default.html')),
-    # url(r'^scoreboard/', include('scoreboard.foo.urls')),
+    url(r'^teams/?$', 'scorebrd.views.teams'),
     url(r'^grappelli/', include('grappelli.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- scorebrd/views.py
+++ scorebrd/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from scorebrd.models import Team
+
+def events(request):
+    events = Events.objects.all()
+    return render_to_response('events.html', {'events': events})
+
+def teams(request):
+    teams = Team.objects.all()
+    return render_to_response('teams.html', {'teams': teams})
