c4fde5cade8e0c870f52a642c7d4cdeea944c3e3
mr.Shu <mr@shu.io>
2013-01-13 23:22:01 +0100

    Added TeamResult to admin; DB redone

--- scorebrd/admin.py
+++ scorebrd/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from scorebrd.models import Team, Group, Competition, Match, Event
+from scorebrd.models import Team, Group, Competition, Match, Event, TeamResult
 
 admin.site.register(Team)
 admin.site.register(Group)
 admin.site.register(Competition)
 admin.site.register(Event)
 admin.site.register(Match)
+admin.site.register(TeamResult)
