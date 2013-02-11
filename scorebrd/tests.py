903f9715d4c2ac9333791649dd36e037adca1e60
Ado Matejov <a.matejov@centrum.sk>
2013-02-11 18:59:23 +0100

    added modeltest

--- scorebrd/tests.py
+++ scorebrd/tests.py
@@ -6,7 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from models import (Team, Event, Group, Competition, LoginForm, Match,
+        TeamResult, MatchSaveForm)
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +15,17 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class ModelTest(TestCase):
+    def test_models(self):
+        self.teamA = Team.objects.create(name="testTeamA")
+        self.teamB = Team.objects.create(name="testTeamB")
+        self.resultA = TeamResult.objects.create(team=self.teamA)
+        self.resultB = TeamResult.objects.create(team=self.teamB)
+        #self.match = Match.objects.create(teamA=self.teamA, teamB=self.teamB, referee=)
+
+        #self.group = Group.objects.create(name="testGroup", teams=[self.teamA, self.teamB, matches=[], results=[self.resultA, self.resultB])
+        #self.competition = Competition.objects.create(name="testCompetition", groups=[self.group])
+        #self.event = Event.objects.create(name="testEvent", competitions=[self.competition])
+
+        self.assertEqual(self.teamA.name, "testTeamA")
